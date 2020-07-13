06329f6597621921d14b431fe0182bc9887af533
fabio <fabio@DESKTOP-9F08P13.homenet.telecomitalia.it>
2020-07-13 13:36:21 +0200

    commit 1

--- src/main/java/it/polito/tdp/imdb/db/ImdbDAO.java
+++ src/main/java/it/polito/tdp/imdb/db/ImdbDAO.java
@@ -5,8 +5,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+
 import it.polito.tdp.imdb.model.Actor;
+import it.polito.tdp.imdb.model.Adiacenza;
 import it.polito.tdp.imdb.model.Director;
 import it.polito.tdp.imdb.model.Movie;
 
@@ -82,6 +86,69 @@ public class ImdbDAO {
 			e.printStackTrace();
 			return null;
 		}
+	}
+
+	public void getDirettori(int anno, Map<Integer, Director> idMap) {
+		
+	
+		String sql = "SELECT DISTINCT  d.id id,d.first_name fname,d.last_name lname " + 
+				"FROM directors d,movies_directors md,movies m " + 
+				"WHERE d.id=md.director_id AND md.movie_id=m.id AND m.year= ? ";
+		
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, anno);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+
+				Director director = new Director(res.getInt("id"), res.getString("fname"), res.getString("lname"));
+				if(!idMap.containsKey(res.getInt("id")))
+				    idMap.put(res.getInt("id"), director);
+			}
+			conn.close();
+			return ;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return ;
+		}
+		
+	}
+
+	public Collection<Adiacenza> getAdiacenze(Map<Integer, Director> idMap) {
+
+	
+		String sql = "SELECT d1.director_id id1,d2.director_id id2 ,COUNT(DISTINCT(r1.actor_id)) as peso " + 
+				"FROM roles r1,roles r2,movies m1,movies m2,movies_directors d1,movies_directors d2 " + 
+				"WHERE r1.actor_id= r2.actor_id AND r1.movie_id>= r2.movie_id AND r1.movie_id=m1.id AND r2.movie_id=m2.id AND d1.movie_id=m1.id AND  d2.movie_id=m2.id AND d1.director_id> d2.director_id " + 
+				"GROUP BY d1.director_id,d2.director_id " + 
+				"";
+		List<Adiacenza> result = new ArrayList<>();
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+
+				if(idMap.containsKey(res.getInt("id1"))&& idMap.containsKey(res.getInt("id2"))) {
+				
+				
+				Adiacenza adiacenza = new Adiacenza(idMap.get(res.getInt("id1")),idMap.get(res.getInt("id2")),res.getDouble("peso"));
+				result.add(adiacenza);
+				}
+			}
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	
+	
 	}
 	
 	
--- src/main/java/it/polito/tdp/imdb/model/Model.java
+++ src/main/java/it/polito/tdp/imdb/model/Model.java
@@ -1,5 +1,52 @@
 package it.polito.tdp.imdb.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.imdb.db.ImdbDAO;
+
 public class Model {
+	
+	private ImdbDAO dao;
+	private Graph<Director,DefaultWeightedEdge> grafo;
+	private Map<Integer,Director>idMap;
+	
+	public Model() {
+		this.dao=new ImdbDAO();
+	}
+	
+	public void creaGrafo(int anno) {
+		
+		this.idMap=new HashMap<Integer,Director>();
+		grafo=new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		this.dao.getDirettori(anno,idMap);
+		Graphs.addAllVertices(grafo, idMap.values());
+		for(Adiacenza a:dao.getAdiacenze(idMap)) {
+			if(!grafo.containsEdge(grafo.getEdge(a.getD1(), a.getD2()))) {
+				
+				Graphs.addEdge(grafo, a.getD1(), a.getD2(), a.getPeso());
+				
+			}
+			
+		}
+	}
+		
+	public int getVertici() {
+		
+			return this.grafo.vertexSet().size();
+		}
+	public int getArchi() {
+		
+		return this.grafo.edgeSet().size();
+	}
+		
+	public Graph<Director,DefaultWeightedEdge> getGrafo(){
+		return this.grafo;
+	}
 
 }
